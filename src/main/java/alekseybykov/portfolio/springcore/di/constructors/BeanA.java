71c9fce6aa5319f5842b66c2b8f654a5762e989a
AlekseyBykov <aleksey.n.bykov@gmail.com>
2020-08-16 09:40:22 +0300

    Added example of DI with using constructor.

---
+++ src/main/java/alekseybykov/portfolio/springcore/di/constructors/BeanA.java
@@ -0,0 +1,27 @@
+package alekseybykov.portfolio.springcore.di.constructors;
+
+/**
+ * @author Aleksey Bykov
+ * @since 16.08.2020
+ */
+public class BeanA {
+
+	private String stringValue;
+	private int intValue;
+
+	public BeanA(String stringValue) {
+		this.stringValue = stringValue;
+	}
+
+	public BeanA(int intValue) {
+		this.intValue = intValue;
+	}
+
+	public String getStringValue() {
+		return stringValue;
+	}
+
+	public int getIntValue() {
+		return intValue;
+	}
+}
--- src/main/java/alekseybykov/portfolio/springcore/di/locators/Locator.java
+++ src/main/java/alekseybykov/portfolio/springcore/di/locators/Locator.java
@@ -1,16 +1,6 @@
 package alekseybykov.portfolio.springcore.di.locators;
 
-import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-import java.io.IOException;
 
 /**
  * @author Aleksey Bykov
