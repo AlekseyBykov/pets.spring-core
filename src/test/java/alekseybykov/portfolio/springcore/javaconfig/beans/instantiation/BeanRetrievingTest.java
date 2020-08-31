50271e7caa46efe83926cfa0e18b72502e515864
AlekseyBykov <aleksey.n.bykov@gmail.com>
2020-08-31 11:13:14 +0300

    Added examples of retrieving beans from container with Java configuration.

---
+++ src/test/java/alekseybykov/portfolio/springcore/javaconfig/beans/instantiation/BeanRetrievingTest.java
@@ -0,0 +1,41 @@
+package alekseybykov.portfolio.springcore.javaconfig.beans.instantiation;
+
+import alekseybykov.portfolio.springcore.javaconfig.beans.BeanA;
+import alekseybykov.portfolio.springcore.javaconfig.config.ApplicationConfig;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Aleksey Bykov
+ * @since 28.08.2020
+ */
+public class BeanRetrievingTest {
+
+	private static ApplicationContext applicationContext;
+
+	@BeforeClass
+	public static void setUp() {
+		applicationContext = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+	}
+
+	@Test
+	public void testRetrieveBeanFromContainer() {
+		BeanA beanA = applicationContext.getBean(BeanA.class);
+		assertEquals("string", beanA.getString());
+	}
+
+	@Test
+	public void testRetrieveBeanFromContainerByUsingDifferentNames() {
+		BeanA beanA = applicationContext.getBean(BeanA.class);
+		BeanA instanceOfBeanA = applicationContext.getBean("anotherNameForBeanA", BeanA.class);
+		BeanA yetAnotherInstanceOfBeanA = applicationContext.getBean("yetAnotherNameForBeanA", BeanA.class);
+
+		assertEquals(beanA, yetAnotherInstanceOfBeanA);
+		assertEquals(instanceOfBeanA, yetAnotherInstanceOfBeanA);
+		assertEquals("string", yetAnotherInstanceOfBeanA.getString());
+	}
+}
